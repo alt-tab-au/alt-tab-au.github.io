b2030e2b35ab833f68fd4620937861ff14faf97d
Rebecca <34409782+siralmat@users.noreply.github.com>
2023-05-04 22:11:19 +0800

    initial commit

---
+++ content/pages/about.md
@@ -0,0 +1,3 @@
+Title: About
+
+*Test page, please ignore*
---
+++ pelicanconf.py
@@ -0,0 +1,23 @@
+AUTHOR = 'alt-tab'
+SITENAME = 'alt-tab.au'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Australia/Perth'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+JINJA_ENVIRONMENT = {'line_statement_prefix': '%'}
